3cbe673ed2491587f2a6a528356351adeb1a0bb0
karen ercoli <karenercoli@gmail.com>
2018-11-21 21:25:59 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,10 @@
 def my_each(array) # put argument(s) here
-counter = 0
-while i<array.count
-  yield
-counter += 1
-end  # code here
+  i = 0
+      while i < array.length
+        yield(array[i])
+        i = i + 1
 end
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
+
+my_each(array) do |i|
   puts i
 end
